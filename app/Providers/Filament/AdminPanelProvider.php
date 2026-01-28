b9b0a93abbdfaa7e028e788183c667ff5bf39106
vgalviz <vgalviz@unal.edu.co>
2026-01-28 16:21:20 -0500

    se actualiza y funcionan los 3 panels

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login(Login::class)
+            ->login()
             ->colors([
                 'primary' => Color::Amber,
             ])

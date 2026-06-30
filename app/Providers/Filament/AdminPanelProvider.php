ac6b77c73bbfce7a040b510f76d0e1fc45c09ccc
vgalviz <vgalviz@unal.edu.co>
2026-06-30 15:39:19 -0500

    quitar modo oscuro por defecto

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->darkMode(false)
 
             // Layout general
             ->maxContentWidth('full')
